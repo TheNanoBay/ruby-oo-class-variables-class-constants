15ce29adecb7f710418594e91306f0f8caeab469
TheNanoBay <noorani.mehdi@outlook.com>
2020-12-10 23:10:49 -0600

    commit tree 5a3f9f5f1bace75c4e99ea2f8377e9ab9216ddf1

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] 
+  BRANDS.uniq
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)  #long hand method would include checking to see if BRANDS.include? == false...then go ahead and push it
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  
 
 end
